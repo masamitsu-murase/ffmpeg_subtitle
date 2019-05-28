33bedbb3f615d7d87289748fbd692bc97986d677
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-05-29 00:22:41 +0900

    Refine base files.

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Sample script to embed subtitle into mpeg.
+
+1. Modify cachedir in conf/fonts.conf.
+2. Modify subtitle.cmd to specify correct filename.
+3. Run subtitle.cmd.
--- subtitle.cmd
+++ subtitle.cmd
@@ -7,6 +7,4 @@ set FC_CONFIG_DIR=%BASE_DIR%conf
 set FONTCONFIG_PATH=%BASE_DIR%conf
 set FONTCONFIG_FILE=%BASE_DIR%conf\fonts.conf
 
-.\ffmpeg.exe -i hoge.mp4 -vf subtitles=hoge.srt:force_style='FontName=Meiryo' -t 20 hoge3.mp4
-
-
+ffmpeg.exe -i hoge.mp4 -vf subtitles=hoge.srt:force_style='FontName=Meiryo' -t 20 hoge3.mp4
