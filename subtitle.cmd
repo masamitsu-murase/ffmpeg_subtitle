c3ef28ae58555d73c48a631e80628af0ac00487f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-05-28 21:06:44 +0900

    Initial commit.

---
+++ subtitle.cmd
@@ -0,0 +1,12 @@
+
+setlocal
+
+set BASE_DIR=%~dp0
+
+set FC_CONFIG_DIR=%BASE_DIR%conf
+set FONTCONFIG_PATH=%BASE_DIR%conf
+set FONTCONFIG_FILE=%BASE_DIR%conf\fonts.conf
+
+.\ffmpeg.exe -i hoge.mp4 -vf subtitles=hoge.srt:force_style='FontName=Meiryo' -t 20 hoge3.mp4
+
+
